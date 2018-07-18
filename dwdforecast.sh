e4833a104853658b486e6607537ff16113083bba
joediboe <41378398+joediboe@users.noreply.github.com>
2018-07-18 15:50:35 +0200

    Create dwdforecast.sh

---
+++ dwdforecast.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+wget -O weather.csv http://opendata.dwd.de/weather/local_forecasts/poi/K2791-MOSMIX.csv
+stunde_aktuell=$(date +%H)
+stunde_aktuell=${stunde_aktuell#0}
+echo $stunde_aktuell
+stunde_00=$((3*($stunde_aktuell/3)))
+echo $stunde_00
+foo=$(printf "%02d" $stunde_00)
+echo $foo
